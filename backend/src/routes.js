6a7f45cdb742a84429c43b60b2109bc239ec0cd1
Francinildo <francis584@gmail.com>
2020-03-24 23:23:57 -0300

    SessionController created

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const express = require("express");
 const OngController = require("./Controllers/OngController");
 const IncidentController = require("./Controllers/IncidentController");
 const ProfileController = require("./Controllers/ProfileController");
+const SessionController = require("./Controllers/SessionController");
 
 const routes = express.Router();
+
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
